62cb6260147b27ca85c8fa05003124e5e8eceb1d
nasir <nu_ahmed@yahoo.com>
2023-12-04 18:35:27 -0800

    terraform module

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-0932daf21fd6e22ba"
+}
+variable "amiID" {
+	default="ami-023c11a32b0207432"
+}
+variable "keyPair" {
+	default="firstkey"
+}
+variable "keyPair1" {
+	default="firstkey.pem"
+}
+
+
+
+
